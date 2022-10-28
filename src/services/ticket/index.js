29f28e87d893846dd1fc4409aabc9a7e59b2d42f
wesleybert <wesleyberto4@gmai.com>
2022-10-28 14:36:24 -0300

    update api date

--- src/services/ticket/index.js
+++ src/services/ticket/index.js
@@ -1,9 +1,9 @@
 import { api } from "../api";
 
 export const TicketService = {
-  buyTicket: (valor, dataCompra, usuario) => api.post('/comprapassagem', { valor, dataCompra, usuario }),
-  deleteTicket: (ticketId) => api.delete(`/comprapassagem/${ticketId}`),
-  updateTicket: (ticketId) => api.put(`/comprapassagem/${ticketId}`),
-  updateTicketStatus: (ticketId) => api.put(`/comprapassagem/${ticketId}/atualizar-status`),
-  searchTicket: (userId) => api.get(`/comprapassagem?usuario=${userId}`),
+  buyTicket: (valor, dataCompra, usuario) => api.post('/passagem', { valor, dataCompra, usuario }),
+  deleteTicket: (ticketId) => api.delete(`/passagem/${ticketId}`),
+  updateTicket: (ticketId) => api.put(`/passagem/${ticketId}`),
+  updateTicketStatus: (ticketId) => api.put(`/passagem/${ticketId}/atualizar-status`),
+  searchTicket: (userId) => api.get(`/passagem?usuario=${userId}`),
 }
--- src/services/user/index.js
+++ src/services/user/index.js
@@ -1,7 +1,6 @@
 import { api } from "../api";
 
 export const UserService = {
-  loginUser: (email, senha) => api.post('/usuarios/login', { email, senha }),
-  registerUser: (email, senha) => api.post('/usuarios', { email, senha }),
-  userBalance: (userId) => api.get(`/usuarios/${userId}/saldo`),
+  loginUser: (email, senha) => api.post('/usuario/login', { email, senha }),
+  registerUser: (email, senha) => api.post('/usuario/create', { email, senha }),
 }
